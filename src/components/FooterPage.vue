9419fea0db379e2da2b5a0091790d155b2f96e58
ablancaa <ablancaa@gmail.com>
2025-06-02 19:59:30 +0200

    Creación de Footer

---
+++ src/components/FooterPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+<template>
+  <footer class="bg-light text-center text-lg-start">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 Copyright:
+      <a class="text-dark" href="https://hospital.vallhebron.com/assistencia/especialitats/cirurgia-plastica-i-cremats">Unitat de Cremats</a>
+    </div>
+  </footer> 
+</template>
+
+
+<style scoped>
+</style>
