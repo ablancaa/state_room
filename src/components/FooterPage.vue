dab6ecad58786bc57e142725570421ae4402dc05
ablancaa <ablancaa@gmail.com>
2025-06-06 20:07:02 +0200

    Border foot

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -2,8 +2,8 @@
 
 </script>
 <template>
-  <footer class="bg-light text-center text-lg-start">
-    <div class="text-center p-3" style="background-color: rgba(27, 0, 255);">
+  <footer class="bg-light text-center text-lg-start ">
+    <div class="text-center p-3 border" style="background-color: rgba(27, 0, 255);">
      <p>© 2025 Copyright: <a class="text-white" href="https://hospital.vallhebron.com/assistencia/especialitats/cirurgia-plastica-i-cremats">Unitat de Cremats</a></p>
      <p>Designer: Alberto Blanca Alcaide</p> 
       
@@ -17,5 +17,8 @@ p,a {
   color: white;
   font-size: 14px;
   margin: 0;
+}
+.border{
+    border-radius: 10px;
 }
 </style>
