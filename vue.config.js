bbbc66830c4061d18fe4463fdb55a6581f892da3
ablancaa <ablancaa@gmail.com>
2026-03-09 00:44:19 +0100

    Cambio de color pwa

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,14 @@ module.exports = defineConfig({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
       })
     ]
+  },
+    pwa: {
+    name: 'Unitat de Cremats', // Nombre de tu aplicación
+    short_name: 'Unitat de Cremats', // Nombre corto para la pantalla de inicio
+    themeColor: '#1B00FF',       // Aquí pones tu color deseado
+    msTileColor: '#1B00FF',      // Para Windows tiles (opcional)
+    manifestOptions: {
+      background_color: '#FFFFFF', // Fondo de splash screen
+    }
   }
 })
