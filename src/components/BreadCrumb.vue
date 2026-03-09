6553fdfc96ca17f1e6a88d513825d07968ed8072
ablancaa <ablancaa@gmail.com>
2026-03-09 15:08:59 +0100

    cambio en breadcrum

--- src/components/BreadCrumb.vue
+++ src/components/BreadCrumb.vue
@@ -2,7 +2,7 @@
   <nav class="breadcrumb" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
     <div v-for="item in breadcrumb" :key="item.name" class="breadcrumb-item">
       <span class="m-3" v-if="item.name=='Home'">{{item.name}}</span>
-      <span class="m-3" v-else><router-link to="/Dashboard">Home / </router-link>{{item.name}}</span>
+      <span class="m-3" v-else><router-link to="/Dashboard">Home | </router-link>{{item.name}}</span>
       
     </div>
   </nav>
